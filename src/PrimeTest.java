d78857f25733c68d241dd64e3febc8f884cd56d4
kandi <kandibandasaigopi446@gmail.com>
2024-02-14 11:54:56 +0530

    hi

---
+++ src/PrimeTest.java
@@ -0,0 +1,14 @@
+import inheritance.Prime;
+import inheritance.PrimeAccount;
+
+public class PrimeTest {
+    public static void main(String[] args){
+        String PrimeAccount;
+        PrimeAccount primeAccount = (PrimeAccount) new PrimeAccount();
+
+
+
+
+    }
+}
+
